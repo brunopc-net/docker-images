92d5f8b41bfda527aea6963e85f3342d0d9caec1
Bruno PC <work@brunopc.net>
2024-10-18 19:27:50 -0400

    New chained selector

--- gradle-jdk/get_latest_gradle.py
+++ gradle-jdk/get_latest_gradle.py
@@ -14,8 +14,9 @@ def get_latest_gradle_version_and_checksum():
     soup = BeautifulSoup(response.text, 'html.parser')
 
     # Find the latest version (assuming it's in a specific format)
-    versions = soup.find_all(string=re.compile(r'v?(\d+\.\d+\.\d+)'))
-    print(f"versions[0]: {versions[0]}")
+    last = soup.find('main').find('h3').get_text()
+
+    print(f"last: {last}")
 
     latest_version = versions[0].split()[1] if versions else None
  
